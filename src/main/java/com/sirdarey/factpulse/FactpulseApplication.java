341930b378f17890b23244f38d981994b495ae2b
sirDarey <darey1oguns@gmail.com>
2025-12-04 16:08:28 +0100

    done

---
+++ src/main/java/com/sirdarey/factpulse/FactpulseApplication.java
@@ -0,0 +1,13 @@
+package com.sirdarey.factpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactpulseApplication.class, args);
+	}
+
+}
